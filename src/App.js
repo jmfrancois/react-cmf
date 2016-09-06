56615b1832aeaf5bb6c3d6af1524378c1593c6a3
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2016-09-06 15:40:25 +0200

    feat: add i18n support

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 
 import RegistryProvider from './RegistryProvider';
 import UIRouter from './UIRouter';
 
 const App = (props) => (
 	<Provider store={props.store}>
-		<RegistryProvider>
-			<UIRouter history={props.history} />
-		</RegistryProvider>
+		<I18nextProvider i18n={props.i18n}>
+			<RegistryProvider>
+				<UIRouter history={props.history} />
+			</RegistryProvider>
+		</I18nextProvider>
 	</Provider>
 );
 
 App.propTypes = {
 	store: React.PropTypes.object.isRequired,
+	i18n: React.PropTypes.object.isRequired,
 	history: React.PropTypes.object,
 };
 
