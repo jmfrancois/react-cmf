b42965256598cb3d9acd178e9e63a8809b773274
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2016-09-09 17:46:29 +0200

    chore: fix test of App

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
-
+import { I18nextProvider } from 'react-i18next';
 import App from '../src/App';
 import RegistryProvider from '../src/RegistryProvider';
 import UIRouter from '../src/UIRouter';
@@ -16,12 +16,15 @@ describe('uiAbstraction App', () => {
 			},
 		};
 		const history = {};
-		const wrapper = shallow(<App store={store} history={history} />);
+		const i18n = {};
+		const wrapper = shallow(<App store={store} history={history} i18n={i18n} />);
 		expect(wrapper.contains(
 			<Provider store={store}>
-				<RegistryProvider>
-					<UIRouter history={history} />
-				</RegistryProvider>
+				<I18nextProvider i18n={i18n}>
+					<RegistryProvider>
+						<UIRouter history={history} />
+					</RegistryProvider>
+				</I18nextProvider>
 			</Provider>)
 		).toEqual(true);
 	});
