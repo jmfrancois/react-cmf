e42550514aa11d71a4e3674f127c3a5c0cd587b4
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2016-09-30 17:05:48 +0200

    doc: add some doc on the provider

--- src/mock/provider.js
+++ src/mock/provider.js
@@ -2,7 +2,28 @@ import React from 'react';
 import mock from './store';
 
 const store = mock.store();
+/**
+ * This component help you to mock the provider.
+ * If you want to write a pure component that use an other which
+ * is connected on CMF, you will need to provide a CMF store.
+ * @example
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-cmf/lib/mock';
 
+import AppMenu from './AppMenu.component';
+
+describe('AppMenu', () => {
+    it('should render its name', () => {
+        const wrapper = renderer.create(
+            <Provider>
+                <AppMenu />
+            </Provider>
+        ).toJSON();
+        expect(wrapper).toMatchSnapshot();
+    });
+});
+ */
 class MockProvider extends React.Component {
 	getChildContext() {
 		return { store };
